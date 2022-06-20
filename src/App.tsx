4328282f6d57183a4058e260e22502673eb8cece
Matheus Rotondano de Camargo <matheusrotondano@gmail.com>
2022-06-20 02:06:09 -0300

    BrowserRouter basename

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { AppWrapper } from './styles';
 export const App: React.FC = () => {
   return (
 		<AppWrapper>
-			<BrowserRouter>
+			<BrowserRouter basename={process.env.PUBLIC_URL}>
 				<Routes>
 					<Route element={<Home/>} path="/" />
 					<Route element={<Details/>} path="/details/:characterId" />
